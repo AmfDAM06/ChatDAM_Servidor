fb34e435c3f08b8506655b9940a640cea986ddc5
AmfDAM06 <alvaromartinezflores06@gmail.com>
2026-05-25 10:58:36 +0200

    Usuario y UsuarioRepository

---
+++ src/main/java/org/iesalandalus/chatdam/server/model/Usuario.java
@@ -0,0 +1,53 @@
+package org.iesalandalus.chatdam.server.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String usuario;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(String usuario, String password) {
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/org/iesalandalus/chatdam/server/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.chatdam.server.repository;
+
+import org.iesalandalus.chatdam.server.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByUsuario(String usuario);
+
+}
