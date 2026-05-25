532ce1302769911f25721ad03b09d723e48c7fc9
AmfDAM06 <alvaromartinezflores06@gmail.com>
2026-05-25 10:22:48 +0200

    Primer Commit

---
+++ src/main/java/org/iesalandalus/chatdam/server/ChatDamServidorApplication.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.chatdam.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatDamServidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatDamServidorApplication.class, args);
+    }
+
+}
